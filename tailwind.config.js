cd85951a8a1052574fb84a640932e2e37c9f5f6f
Baraka Mukelenga <barackmukelenga100@gmail.com>
2022-08-22 23:39:40 +0200

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: '#0e1630',
+      secondary: '#ed4a4a',
+      text: '#fff',
+    },
     extend: {},
   },
   plugins: [],
