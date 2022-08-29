3c44e36dea8aa877dacc4cc2a1e833a508392ad4
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-08-29 02:55:27 +0200

    Update styling

--- Components/Page/Page.tsx
+++ Components/Page/Page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = (props: { children: React.ReactNode; className?: string }) => {
   const { children, className } = props;
   return (
-    <main className={`p-5 sm:p-[2rem] lg:p-[8rem] lg:pt-20 ${className || ''}`}>
+    <main className={`p-5 sm:p-28 lg:p-44 lg:pt-20 ${className || ''}`}>
       {children}
     </main>
   );
