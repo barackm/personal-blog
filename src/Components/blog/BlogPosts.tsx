d8e79abfae0526da69e966a39895755c3c9a60b0
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-12-12 01:30:38 +0200

    Add the article interface

--- src/Components/blog/BlogPosts.tsx
+++ src/Components/blog/BlogPosts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IArticle } from '../../types/article';
 import BlogItem from './BlogPost';
 
 type BlogPostList = {
@@ -9,7 +10,7 @@ const BlogPosts = (props: BlogPostList) => {
   const { posts } = props;
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
-      {posts.map((post: any) => (
+      {posts.map((post: IArticle) => (
         <BlogItem key={post._id} post={post} />
       ))}
     </div>
